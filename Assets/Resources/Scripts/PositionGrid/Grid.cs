61eb0f3a3d22c22f9a64ae2b6b3451ce80380cf9
caramelosorico18 <lordominatorulez@gmail.com>
2026-05-22 19:50:25 +0100

    Avance con la grid, da fallos

--- Assets/Resources/Scripts/PositionGrid/Grid.cs
+++ Assets/Resources/Scripts/PositionGrid/Grid.cs
@@ -8,21 +8,28 @@ public class Grid {
    private int height;
    private int depth;
    private int[,,] gridArray;
+   private TextMesh[,] debugTextArray;
    private float cellSize;
-   public Grid(int width, int height, int depth, float cellSize)
+   private Vector3 originPosition;
+    public Grid(int width, int height, int depth, float cellSize, Vector3 originPosition)
     {
         this.width = width;
         this.depth = depth;
         this.height = height;
         this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
         gridArray = new int[width, height, depth];
+        debugTextArray = new TextMesh[width, depth];
         //Debug.Log("Width: " + width + "Height: " + height);
-        for(int x = 0; x<gridArray.GetLength(0); x++)
+
+        for (int x = 0; x<gridArray.GetLength(0); x++)
         {
             for(int z=0; z<gridArray.GetLength(2); z++)
             {
                 gridArray[x, 0, z] = 0;
-                UtilsClass.CreateWorldText(gridArray[x, 0, z].ToString(), null, GetWorldPosition(x, z), 20, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x, z] = UtilsClass.CreateWorldText(gridArray[x, 0, z].ToString(), null, GetWorldPosition(x, z) + new Vector3(cellSize, 0, cellSize) * 0.5f, 20, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x, z].transform.rotation = Quaternion.Euler(90, 0, 0);
                 Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x + 1, z), Color.white, 100f);
                 Debug.Log("X: " + x + "Z: " + z);
@@ -30,9 +37,48 @@ public class Grid {
         }
         Debug.DrawLine(GetWorldPosition(0, depth), GetWorldPosition(width, depth), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, depth), Color.white, 100f);
+        SetValue(2, 1, 56);
     }
     private Vector3 GetWorldPosition(int x, int z)
     {
-        return new Vector3(x, 1, z) * cellSize;
+        return new Vector3(x, 1, z) * cellSize + originPosition;
+    }
+    private void GetXZ(Vector3 worldPosition, out int x, out int z)
+    {
+        x = Mathf.FloorToInt((worldPosition.x - originPosition.x) / cellSize);
+        z = Mathf.FloorToInt((worldPosition.z - originPosition.z) / cellSize);
+    }
+
+    public void SetValue(int x, int z, int value)
+    {
+        if(x>= 0 && z >= 0 && x < width && z < depth)
+        {
+            gridArray[x, 0, z] = value;
+            debugTextArray[x, z].text = gridArray[x, 0, z].ToString();
+            debugTextArray[x, z].transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+    }
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, z;
+        GetXZ(worldPosition, out x, out z);
+        SetValue(x, z, value);
+    }
+    public int GetValue(int x, int z)
+    {
+        if (x >= 0 && z >= 0 && x < width && z < depth)
+        {
+            return gridArray[x, 0, z];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, z;
+        GetXZ(worldPosition, out x, out z);
+        return GetValue(x, z);
     }
 }
--- Assets/Resources/Scripts/PositionGrid/Test.cs
+++ Assets/Resources/Scripts/PositionGrid/Test.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Test : MonoBehaviour
 {
-   private void Start()
+    private Grid grid;
+    private void Start()
     {
-        Grid grid = new Grid(20, 1, 10, 10f);
+        grid = new Grid(4, 1, 3, 10f, new Vector3(20, 5, 0));
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 55);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
     }
 }
+

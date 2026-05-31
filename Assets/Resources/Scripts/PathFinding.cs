05e326b69ea03d2dc1d495fc8d42d7cb19d8a496
caramelosorico18 <lordominatorulez@gmail.com>
2026-05-31 16:19:45 +0100

    Optimization failed, reverted and noted

--- Assets/Resources/Scripts/PathFinding.cs
+++ Assets/Resources/Scripts/PathFinding.cs
@@ -22,13 +22,23 @@ public class PathFinding : MonoBehaviour {
         sw.Start();
         Node startNode = grid.NodeFromWorldPoint(startPos);
         Node targetNode = grid.NodeFromWorldPoint(targetPos);
-        Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+        List<Node> openSet = new List<Node>(); //Opt: Replace both List with Heap and pass grid.MaxSize
         HashSet<Node> closedSet = new HashSet<Node>();
         openSet.Add(startNode);
 
         while (openSet.Count > 0)
         {
-            Node currentNode = openSet.RemoveFirst();
+            Node currentNode = openSet[0];
+            for(int i = 1; i < openSet.Count; i++)
+            {
+                if(openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+            openSet.Remove(currentNode);
+            //Opt: Replace above until start of while with: Node currentNode = openSet.RemoveFirst();
+
             closedSet.Add(currentNode);
 
             if (currentNode == targetNode)

c0b0ec04d2415ef82d8dc755f56c4c14cc214194
caramelosorico18 <lordominatorulez@gmail.com>
2026-06-09 21:21:51 +0100

    Camara hecha, movimiento sin acabar

---
+++ Assets/Resources/Scripts/Player/MouseController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+public float mouseSensitivity = 300f;
+float xRotation = 0f;
+float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //*??
+        xRotation -= mouseY;
+
+        //Max Rotation
+        xRotation = Mathf.Clamp(xRotation, -85f, 85f);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
--- Assets/Resources/Scripts/Player/PlayerController.cs
+++ Assets/Resources/Scripts/Player/PlayerController.cs
@@ -7,35 +7,106 @@ public class PlayerController : MonoBehaviour
 
 /*Esta linea hace que funciones todo el script, todo debe estas dentro de sus llaves*/
 {
-    private Vector3 lastPosition;
-    private Rigidbody player;
-    public float speedRun = 4f;
-    private Vector3 moveInput;
+    public Rigidbody2D player;
+    public float inputMovimientoH; /*Registra inputs*/
+    public float inputMovimientoV;
+
+    public float speed;
+    public float jumpHeight;
+    public float health;
+    public float gravity = -19f;
+    public GroundedController groundCheck;
+    public bool isGrounded;
+    public float groundDistance;
+    public LayerMask groundMask;
+    public enum direction;
+    public PlayerStats playerStats;
+    public Animator PyAnims;
+    Vector3 velocity;
+    bool isMoving;
+    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
+    
+    public enum direction{none, w, d, s, a, wd, Wa, sd, sa}
     void Start()
     {
-
-        player = GetComponent<Rigidbody>(); /*El objeto se establece a si mismo como el player*/
+        player = GetComponent<Rigidbody2D>();
+        health = playerStats.MaxHealth;
+        speed = playerStats.SpeedRun;
+        jumpHeight = playerStats.SpeedJump;
     }
 
     // Update is called once per frame
     void Update()
     {
-        lastPosition = new Vector3(player.position.x, player.position.y, player.position.z);
-        Vector3 mousePos = Input.mousePosition;
-        moveInput = new Vector3(mousePos.x, 0, mousePos.z);
-
-
-        if (Input.GetMouseButtonDown(2))
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        ProcesarMovimiento();
+        if(isGrounded && velocity.y < 0)
         {
-            player.velocity = new Vector3((lastPosition.x + moveInput.x) * speedRun, 0, (lastPosition.z + moveInput.z) * speedRun);
+            velocity.y = -2f;
         }
-        if (player.position == moveInput)
+
+        
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            player.velocity = new Vector3(0, 0, 0);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Falling
+        velocity.y = gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        if(lastPosition != gameObject.transform.position && isGrounded == true)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
         }
     }
-    private void FixedUpdate()
+
+    void ProcesarMovimiento()
     {
-        player.MovePosition(player.position + moveInput.normalized * speedRun * Time.fixedDeltaTime);
+        float inputMovimientoH = Input.GetAxis("Horizontal");
+        float inputMovimientoV = Input.GetAxis("Vertical");
+        player.velocity = new Vector3(inputMovimientoH, inputMovimientoV, player.velocity.y);
+        //PyAnims.SetBool("Walking", true);
+        //if(inputMovimientoH == 0 && inputMovimientoV == 0){PyAnims.SetBool("Walking", false);}
+    }
+    void FixedUpdate()
+    {
+        switch (Input.GetKey)
+        {
+            //Cambiar(enum direction)*
+            case input.GetKey("w"):
+            PyAnims.SetBool("WalkingFw", true);
+            direction = 1;
+
+            case input.GetKey("d"):
+            PyAnims.SetBool("WalkingRg", true);
+            direction = 2;
+
+             case input.GetKey("s"):
+            PyAnims.SetBool("WalkingBw", true);
+            direction = 3;
+
+            case input.GetKey("a"):
+            PyAnims.SetBool("WalkingLf", true);
+            direction = 4;
+
+            case input.GetKey("w"):
+            PyAnims.SetBool("WalkingFw", true);
+            direction = 1;
+        }
+        switch (direction)
+        {
+            case direction == 1:
+
+        }
     }
 }

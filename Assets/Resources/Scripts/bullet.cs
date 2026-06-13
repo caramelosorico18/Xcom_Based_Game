43b964a6b360d7877d4a9140db1fa23798d3534a
caramelosorico18 <lordominatorulez@gmail.com>
2026-06-13 19:15:33 +0100

    Imported more assets, making breakable boxes on blender

--- Assets/Resources/Scripts/bullet.cs
+++ Assets/Resources/Scripts/bullet.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision objectWeHit)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (objectWeHit.gameObject.CompareTag("Wall"))
         {
-            print("hit" + collision.gameObject.name + " !");
+            print("Wall hit!");
+            CreatBulletImpactEffect(objectWeHit);
+            Destroy(gameObject);
+        }
+        if (objectWeHit.gameObject.CompareTag("Enemy"))
+        {
+            print("hit" + objectWeHit.gameObject.name + " !");
         }
         // Destruye la bala al colisionar con cualquier objeto
         Destroy(gameObject);
+    }
+
+    void CreatBulletImpactEffect(Collision objectWeHit)
+    {
+        ContactPoint contact = objectWeHit.contacts[0];
+        GameObject hole = Instantiate(
+            GlobalReferences.Instance.bulletImpactEffectPrefab, contact.point, Quaternion.LookRotation(contact.normal)
+        );
+        hole.transform.SetParent(objectWeHit.gameObject.transform);
     }
 }
